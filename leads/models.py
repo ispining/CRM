580900874744595eac6924e86644b8468403a7c3
Anonymous <ilvi142014@gmail.com>
2022-12-02 09:40:00 +0200

    Test commit

--- leads/models.py
+++ leads/models.py
@@ -7,6 +7,9 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     # agent = models.ForeignKey("", on_delete=models.CASCADE)
 
+
+
+
 class Worker(models.Model):
     user_id = models.CharField(max_length=10)
     username = models.CharField(max_length=20)
@@ -24,3 +27,21 @@ class Worker(models.Model):
     birth_date = models.CharField(max_length=10)
     reg_date = models.CharField(max_length=10)
     age = models.IntegerField(default=0)
+
+
+class Agent(models.Model):
+    affiliate_id = models.CharField(max_length=10)
+    affiliate_name = models.CharField(max_length=20)
+    parent_company = models.CharField(max_length=20)
+    staff_ids = models.CharField(max_length=100)
+    affiliate_country = models.CharField(max_length=20)
+    affiliate_city = models.CharField(max_length=20)
+    affiliate_location = models.CharField(max_length=50)
+    affiliate_percent = models.CharField(max_length=2)
+    phone1 = models.CharField(max_length=13)
+    phone2 = models.CharField(max_length=13)
+    email = models.CharField(max_length=30)
+    site = models.CharField(max_length=30)
+    status = models.CharField(max_length=15)
+    contact_id = models.CharField(max_length=10)
+    reg_date = models.CharField(max_length=20)
