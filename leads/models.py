74bc236561651cb2fc84ae3804363f8ace80b017
Anonymous <ilvi142014@gmail.com>
2022-12-02 09:46:49 +0200

    Test commit

--- leads/models.py
+++ leads/models.py
@@ -45,3 +45,22 @@ class Agent(models.Model):
     status = models.CharField(max_length=15)
     contact_id = models.CharField(max_length=10)
     reg_date = models.CharField(max_length=20)
+
+
+class Clients(models.Model):
+    user_id = models.CharField(max_length=10)
+    username = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    teudat_zeut = models.CharField(max_length=13)
+    status = models.CharField(max_length=10)
+    vip = models.CharField(max_length=10)
+    country = models.CharField(max_length=120)
+    city = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    phone1 = models.CharField(max_length=13)
+    phone2 = models.CharField(max_length=13)
+    email = models.CharField(max_length=30)
+    birth_date = models.CharField(max_length=20)
+    reg_date = models.CharField(max_length=20)
+    family_ids = models.CharField(max_length=150)
